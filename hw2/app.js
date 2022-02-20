f1b144d4ae64ffcfaf1887d5a59e252394c9ee29
tar04 <taras200195@gmail.com>
2022-02-20 16:26:57 +0200

    refactored code

--- hw2/app.js
+++ hw2/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {engine} = require('express-handlebars');
 const path = require('path');
 
-let users = []
+let users = [];
 
 const app = express();
 
@@ -15,42 +15,47 @@ app.set('views', path.join(__dirname, 'static'));
 
 
 app.get('/login', (req, res) => {
-    res.render('login')
-})
+    res.render('login');
+});
 
 app.get('/users', (req, res) => {
     const {age, city} = req.query;
     if (age || city) {
-        let newUsers = [...users]
+        let newUsers = [...users];
+
         if (city) {
-            newUsers = newUsers.filter(user => user.city === city)
+            newUsers = newUsers.filter(user => user.city === city);
         }
+
         if (age) {
-            newUsers = newUsers.filter(user => user.age === age)
+            newUsers = newUsers.filter(user => user.age === age);
         }
+
         res.render('users', {users: newUsers});
     }
     res.render('users', {users});
-})
+});
 
 app.get('/users/:id', ({params: {id}}, res) => {
     const normalId = Number(id);
     const user = users.find(user => user.id === normalId);
+
     if (user) {
-        res.render('user', {user})
+        res.render('user', {user});
         return;
     }
+
     res.render('notFound');
 
-})
+});
 
 app.get('/signin', (req, res) => {
     res.render('signin');
-})
+});
 
 app.get('/error', (req, res) => {
     res.render('error');
-})
+});
 
 app.get('/deleteUser/:id', ({params:{id}}, res) => {
     const normalId = Number(id);
@@ -60,31 +65,37 @@ app.get('/deleteUser/:id', ({params:{id}}, res) => {
 
 app.post('/signin', (req, res) => {
     const {email, password} = req.body;
+
     if (users.length) {
         users.forEach(user => {
+
             if (((user.email === email) && (user.password === password))) {
-                res.redirect(`/users/${user.id}`)
+                res.redirect(`/users/${user.id}`);
             }
+
         })
     }
+
     res.render('notFound');
-})
+});
 
 app.post('/login', (req, res) => {
     for (const user of users) {
+
         if (user.email === req.body.email) {
-            res.redirect('/error')
+            res.redirect('/error');
             return;
         }
+
     }
     users.push({...req.body, id: users.length ? users[users.length - 1].id + 1 : 1});
     res.redirect('/users');
-})
+});
 
 app.use((req, res) => {
-    res.render('notFound')
-})
+    res.render('notFound');
+});
 
 app.listen(5200, () => {
     console.log('Server has started on port http://localhost:5200');
-})
+});
